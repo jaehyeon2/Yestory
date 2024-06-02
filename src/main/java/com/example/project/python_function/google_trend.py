08d2ce10c6e487c6b95859e67ca979a2d677b7c9
jaehyeon2 <wol5971@gmail.com>
2024-06-02 22:18:49 +0900

    generate trend file.csv with python script

--- src/main/java/com/example/project/python_function/google_trend.py
+++ src/main/java/com/example/project/python_function/google_trend.py
@@ -9,10 +9,5 @@ pytrends = TrendReq(hl='ko-KR', tz=540)
 # 데이터 가져오기 (최근 24시간)
 trending_searches_df = pytrends.trending_searches(pn='south_korea')
 
-# 파일명에 날짜 추가
-file_name = f'trending_google.csv'
-
 # CSV 파일로 저장
 trending_searches_df.to_csv(file_name, index=False, header=['검색어'])
-
-print(f"데이터가 '{file_name}' 파일에 저장되었습니다.")
--- src/main/java/com/example/project/service/impl/CrawlingServiceImpl.java
+++ src/main/java/com/example/project/service/impl/CrawlingServiceImpl.java
@@ -1,19 +1,14 @@
 package com.example.project.service.impl;
 
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,45 +22,77 @@ public class CrawlingServiceImpl implements CrawlingService{
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
-	private String GOOGLE_TREND_URL;
-	
-	@Autowired
 	private String GOOGLE_TREND_FILE_PATH;
 	
 	@Autowired
 	private String GOOGLE_TREND_FILE_NAME_HEAD;
 	
-	@Override
-	public List<String> crawlGoogleSearchTrendList() throws Exception{
-		
-		List<String> trendList = new ArrayList<>();
-		logger.info("GOOGLE_TREND_URL = {}", GOOGLE_TREND_URL);
-		logger.info("GOOGLE_TREND_FILE_PATH = {}", GOOGLE_TREND_FILE_PATH);
-		logger.info("GOOGLE_TREND_FILE_NAME_HEAD = {}", GOOGLE_TREND_FILE_NAME_HEAD);
-		
-		try{
-			String yesterdayString = LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-			logger.info("yesterday = {}", yesterdayString);
-			
-			StringBuilder filePath = new StringBuilder();
-			filePath
-				.append(GOOGLE_TREND_FILE_PATH)
-				.append(GOOGLE_TREND_FILE_NAME_HEAD)
-				.append(yesterdayString)
-				.append(".csv");
-			
-//			ProcessBuilder pb = new ProcessBuilder(/python_function, pythonScript, filaPath);
-//            Process process = pb.start();
-			
-			logger.info("filePath = {}", filePath.toString());
-			
-		}catch(Exception e){
-			logger.error("error = {}", e.getMessage());
-		}
-		
-		return trendList;
-		
-	}
+	@Autowired
+	private String GOOGLE_TREND_PYTHON_FILE_PATH;
 	
+	@Override
+	public List<String> crawlGoogleSearchTrendList() throws Exception {
+	    List<String> trendList = new ArrayList<>();
+	    
+	    try {
+	        String yesterdayString = LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+	        logger.info("CrawlingServiceImpl::crawlGoogleSearchTrendList::yesterday = {}", yesterdayString);
+	        
+	        StringBuilder sbFilePath = new StringBuilder();
+	        String filePath = sbFilePath
+	            .append(GOOGLE_TREND_FILE_PATH)
+	            .append(GOOGLE_TREND_FILE_NAME_HEAD)
+	            .append(yesterdayString)
+	            .append(".csv")
+	            .toString();
+	        
+	        this.generateTrendToCsv(filePath);
+	        trendList = this.getTrendListFromCsv(filePath);
+	        for (String trend:trendList){
+	        	logger.info("trend = {}", trend);
+	        }
+	        
+	    } catch (Exception e) {
+	        logger.error("CrawlingServiceImpl::crawlGoogleSearchTrendList::error = {}", e.getMessage(), e);
+	    }
+	    
+	    return trendList;
+	}
+
+	private void generateTrendToCsv(String filePath) throws Exception {
+	    ProcessBuilder pb = new ProcessBuilder("python", GOOGLE_TREND_PYTHON_FILE_PATH, filePath);
+	    Process process = pb.start();
+	    
+	    // Python script error message check
+	    try (BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
+	        StringBuilder errorMessage = new StringBuilder();
+	        String errorLine;
+	        while ((errorLine = errorReader.readLine()) != null) {
+	            errorMessage.append(errorLine).append("\n");
+	        }
+	        
+	        // exit process
+	        int exitCode = process.waitFor();
+	        if (exitCode != 0) {
+	            // Case: process is fail
+	            throw new RuntimeException("Python Script Execution Failed with error: " + errorMessage.toString());
+	        }
+	    }
+	}
+
+	private List<String> getTrendListFromCsv(String filePath) throws Exception {
+	    List<String> trendList = new ArrayList<>();
+	    
+	    try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+	        String line;
+	        // initial line skip
+	        line = br.readLine();
+	        while ((line = br.readLine()) != null) {
+	            trendList.add(line);
+	        }
+	    }
+	    
+	    return trendList;
+	}
 
 }
