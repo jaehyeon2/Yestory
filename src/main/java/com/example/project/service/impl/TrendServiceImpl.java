d362d23c731f0f4ce24ce1f115af32d3a12b21cf
jaehyeon2 <wol5971@gmail.com>
2024-06-09 18:27:39 +0900

    news info crawling

--- src/main/java/com/example/project/service/impl/TrendServiceImpl.java
+++ src/main/java/com/example/project/service/impl/TrendServiceImpl.java
@@ -12,9 +12,11 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.project.service.TrendService;
 
+@Service
 public class TrendServiceImpl implements TrendService {
 
 final Logger logger = LoggerFactory.getLogger(this.getClass());
