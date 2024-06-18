c700e064535b637acffa4d99a0358f85c84bdffd
jaehyeon2 <wol5971@gmail.com>
2024-06-19 01:30:44 +0900

    summary query

--- doc/create-table.sql
+++ doc/create-table.sql
@@ -6,14 +6,13 @@ CREATE TABLE if NOT EXISTS yestory.m_trend
 	insert_date TIMESTAMP NOT NULL,
 	update_date TIMESTAMP NOT NULL,
 	mt_status CHAR(1) NULL,
-	mt_deleted CHAR(1) NOT NULL,
+	mt_delete CHAR(1) NOT NULL,
 	PRIMARY KEY (mt_no)
 )ENGINE=InnoDB default CHARSET=utf8;
 
 CREATE TABLE if NOT EXISTS yestory.m_news
 (
 	mn_no INTEGER NOT NULL AUTO_INCREMENT,
-	mt_no INTEGER NOT NULL,
 	mt_trend VARCHAR(100) NOT NULL,
 	mn_title VARCHAR(100) NOT NULL,
 	mn_content VARCHAR(5000) NOT NULL,
@@ -22,7 +21,7 @@ CREATE TABLE if NOT EXISTS yestory.m_news
 	insert_date TIMESTAMP NOT NULL,
 	update_date TIMESTAMP NOT NULL,
 	mn_status CHAR(1) NULL,
-	mn_deleted CHAR(1) NOT NULL,
+	mn_delete CHAR(1) NOT NULL,
 	PRIMARY KEY (mn_no)
 )ENGINE=InnoDB default CHARSET=utf8;
 
@@ -37,6 +36,6 @@ CREATE TABLE if NOT EXISTS yestory.m_summary
 	insert_date TIMESTAMP NOT NULL,
 	update_date TIMESTAMP NOT NULL,
 	ms_status CHAR(1) NULL,
-	ms_deleted CHAR(1) NOT NULL,
+	ms_delete CHAR(1) NOT NULL,
 	PRIMARY KEY (ms_no)
 )ENGINE=InnoDB default CHARSET=utf8;
