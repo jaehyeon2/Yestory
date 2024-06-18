b3e7016f14f5473f54da6f38d18fd2e1acb69d6f
jaehyeon2 <wol5971@gmail.com>
2024-06-19 00:53:43 +0900

    add parameter

--- src/main/java/com/example/project/beans/model/YSummaryModel.java
+++ src/main/java/com/example/project/beans/model/YSummaryModel.java
@@ -4,6 +4,33 @@ public class YSummaryModel extends BasicModel{
 	
 	private static final long serialVersionUID = 1L;
 	
-	//TODO
-
+	private String mtTrend;
+	private String msTitle;
+	private String msSummary;
+	private String msUrl;
+	
+	public String getMtTrend() {
+		return mtTrend;
+	}
+	public void setMtTrend(String mtTrend) {
+		this.mtTrend = mtTrend;
+	}
+	public String getMsTitle() {
+		return msTitle;
+	}
+	public void setMsTitle(String msTitle) {
+		this.msTitle = msTitle;
+	}
+	public String getMsSummary() {
+		return msSummary;
+	}
+	public void setMsSummary(String msSummary) {
+		this.msSummary = msSummary;
+	}
+	public String getMsUrl() {
+		return msUrl;
+	}
+	public void setMsUrl(String msUrl) {
+		this.msUrl = msUrl;
+	}
 }
