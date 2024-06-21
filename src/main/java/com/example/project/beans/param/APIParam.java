2f1c7173871eb7e22f24a56ca18c03067493c139
jaehyeon2 <wol5971@gmail.com>
2024-06-21 21:03:20 +0900

    kakao chatbot skill test

---
+++ src/main/java/com/example/project/beans/param/APIParam.java
@@ -0,0 +1,17 @@
+package com.example.project.beans.param;
+
+public class APIParam extends BasicParam{
+	
+	private String request;
+
+	public String getRequest() {
+		return request;
+	}
+
+	public void setRequest(String request) {
+		this.request = request;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/example/project/controller/APIcontroller.java
@@ -0,0 +1,26 @@
+package com.example.project.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.project.beans.model.YNewsModel;
+
+@Controller
+@RequestMapping("/api")
+public class APIcontroller {
+	
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@RequestMapping(value={"/request"}, headers="Accept=application/json")
+	public YNewsModel test(@RequestBody String request) throws Exception{
+		
+		YNewsModel news = new YNewsModel();
+		
+		news.setMnTitle("test");
+		
+		return news;
+	}
+}
