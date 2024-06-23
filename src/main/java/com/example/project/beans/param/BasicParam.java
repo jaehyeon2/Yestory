2bea242ce22a93ff7f609a0a588b887e928d3119
jaehyeon2 <wol5971@gmail.com>
2024-06-24 00:32:05 +0900

    summary logic

--- src/main/java/com/example/project/beans/param/BasicParam.java
+++ src/main/java/com/example/project/beans/param/BasicParam.java
@@ -4,6 +4,7 @@ package com.example.project.beans.param;
 public class BasicParam{
 
 	private String history;
+	private String number;
 
 	public String getHistory() {
 		return history;
@@ -11,5 +12,11 @@ public class BasicParam{
 
 	public void setHistory(String history) {
 		this.history = history;
-	}	
+	}
+	public String getNumber(){
+		return number;
+	}
+	public void setNumber(String number){
+		this.number = number;
+	}
 }
