8d4dfa23633707482ca517baf5e2ea3c3d730ea7
jaehyeon2 <wol5971@gmail.com>
2024-07-11 23:18:59 +0900

    logging

--- src/main/java/com/example/project/controller/APIcontroller.java
+++ src/main/java/com/example/project/controller/APIcontroller.java
@@ -25,7 +25,7 @@ public class APIcontroller {
 	@PostMapping(value={"/trendList"})
 	@ResponseBody
 	public ResponseModel trendRequest(@RequestBody RequestParam requestParam) throws Exception {
-		
+		logger.info("APIController::trendRequest::info = API-trendList");
 		ResponseModel response = chatbotAPIService.getResponseOfTrendList(requestParam);
 
         return response;
@@ -34,7 +34,7 @@ public class APIcontroller {
 	@PostMapping(value={"/summary"})
 	@ResponseBody
 	public ResponseModel summaryPostRequest(@RequestBody RequestParam requestParam) throws Exception{
-		
+		logger.info("APIController::summaryPostRequest::info = API-summary");
 		ResponseModel response = chatbotAPIService.getResponseOfSummary(requestParam);
 		
 		return response;
